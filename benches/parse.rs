efe04fa21e8ad78ba1aebae1eeb17c46bbe7bf06
Ilya Zlobintsev <ilya.zl@protonmail.com>
2024-01-06 15:35:32 +0200

    feat: search

--- benches/parse.rs
+++ benches/parse.rs
@@ -12,3 +12,20 @@ fn parse_embedded() -> Database {
     let cursor = Cursor::new(DB_DATA);
     Database::parse_db(cursor).unwrap()
 }
+
+#[divan::bench]
+fn find_polaris() -> Option<String> {
+    let cursor = Cursor::new(DB_DATA);
+    pciid_parser::find_device_name_with_reader(cursor, 0x1002, 0x67df).unwrap()
+}
+
+#[divan::bench]
+fn find_end() -> Option<String> {
+    let cursor = Cursor::new(DB_DATA);
+    pciid_parser::find_device_name_with_reader(cursor, 0x1fc9, 0x3010).unwrap()
+}
+
+#[divan::bench]
+fn find_end_in_file() -> Option<String> {
+    pciid_parser::find_device_name(0x1fc9, 0x3010).unwrap()
+}
